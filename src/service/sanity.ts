891aedddc33a44c8d030b33a05eb29dd5483a54f
slevme <hby2233@naver.com>
2023-04-22 23:08:01 +0900

    delete comments

--- src/service/sanity.ts
+++ src/service/sanity.ts
@@ -1,15 +1,3 @@
-// import { createClient, type ClientConfig } from '@sanity/client';
-
-// const config:ClientConfig = {
-//   projectId: process.env.SANITY_PROJECT_ID,
-//   dataset: process.env.SANITY_DATASET,
-//   useCdn: false,
-//   apiVersion: '2023-04-22',
-//   token: process.env.SANITY_SECRET_TOKEN,
-// };
-
-// const client = createClient(config)
-
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
