444b43abf9fbd0f191b9e5e4a7d1b08314c96dfc
slevme <hby2233@naver.com>
2023-07-04 21:43:34 +0900

    Add metadata

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -3,6 +3,12 @@ import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { redirect } from 'next/navigation';
 import Signin from '@/components/Signin';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Signin',
+  description: 'Signup or Login to Instantgram',
+};
 
 type Props = {
   searchParams: {
--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,14 +1,18 @@
 import UserPosts from '@/components/UserPosts';
 import UserProfile from '@/components/UserProfile';
 import { getUserForProfile } from '@/service/user';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
+import { cache } from 'react';
 
 type Props = {
   params: { username: string };
 };
 
+const getUser = cache(async (username: string) => getUserForProfile(username));
+
 export default async function UserPage({ params: { username } }: Props) {
-  const user = await getUserForProfile(username);
+  const user = await getUser(username);
 
   if (!user) {
     notFound();
@@ -20,4 +24,14 @@ export default async function UserPage({ params: { username } }: Props) {
       <UserPosts user={user} />
     </section>
   );
+}
+
+export async function generateMetadata({
+  params: { username },
+}: Props): Promise<Metadata> {
+  const user = await getUser(username);
+  return {
+    title: `${user?.name} (@${user?.username})`,
+    description: `${user?.name}'s all Instantgram posts`,
+  };
 }
