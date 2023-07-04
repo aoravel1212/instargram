af25cba2ee4e3e76722e0858a76760869b46cd9d
slevme <hby2233@naver.com>
2023-07-04 23:58:23 +0900

    Add a route segment option : force-dynamic

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,4 +1,12 @@
 import UserSearch from '@/components/UserSearch';
+import { Metadata } from 'next';
+
+export const dynamic = 'force-dynamic';
+
+export const metadata: Metadata = {
+  title: 'User Search',
+  description: 'Search users to follow',
+};
 
 export default function SearchPage() {
   return <UserSearch />;
