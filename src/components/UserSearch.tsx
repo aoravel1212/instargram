763cfdfd67bf0c0aa3ad188793cdf721c2877384
slevme <hby2233@naver.com>
2023-07-09 17:40:23 +0900

    change: hooks folder path (src/util/hooks -> src/hooks)

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -5,7 +5,7 @@ import { FormEvent, useState } from 'react';
 import useSWR from 'swr';
 import GridSpinner from './ui/GridSpinner';
 import UserCard from './UserCard';
-import useDebounce from '@/util/hooks/debounce';
+import useDebounce from '@/hooks/debounce';
 
 export default function UserSearch() {
   const [keyword, setKeyword] = useState('');
