043f3054d482ead7e8892a6b3bda89a075351cd1
slevme <hby2233@naver.com>
2023-08-25 01:21:28 +0900

    refactor: Change the data fetching method from direct server requests to execution of the useMe hook

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,7 +1,7 @@
 'use client';
-import { HomeUser, ProfileUser } from '@/model/user';
-import useSWR from 'swr';
+import { ProfileUser } from '@/model/user';
 import Button from './ui/Button';
+import useMe from '@/hooks/me';
 
 type Props = {
   user: ProfileUser;
@@ -9,7 +9,7 @@ type Props = {
 
 export default function FollowButton({ user }: Props) {
   const { username } = user;
-  const { data: loggedInUser } = useSWR<HomeUser>('/api/me');
+  const { user: loggedInUser } = useMe();
 
   // 로그인한 유저의 페이지일 경우 팔로우 버튼 보여줄 필요가 없음
   // 로그인한 유저가 있고 로그인한 유저의 username이 현재 전달된 유저(보고있는 유저페이지의) username이 아니라면 버튼 활성화
--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,23 +1,13 @@
 'use client';
-import { HomeUser, AuthUser } from '@/model/user';
 import Link from 'next/link';
 import { PropagateLoader } from 'react-spinners';
-import useSWR from 'swr';
+import useMe from '@/hooks/me';
 import Avatar from './Avatar';
 import ScrollableBar from './ui/ScrollableBar';
 
-type Props = {
-  user: AuthUser;
-};
-
 export default function FollowingBar() {
-  const { data, isLoading: loading, error } = useSWR<HomeUser>('/api/me');
-  const users = data?.following && [
-    ...data.following,
-    ...data.following,
-    ...data.following,
-    ...data.following,
-  ];
+  const { user, isLoading: loading, error } = useMe();
+  const users = user?.following;
 
   return (
     <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto relative z-0">
