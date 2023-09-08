0d6bcc1cdbdf1d09e062a0470115b1eb63b3ac7e
slevme <hby2233@naver.com>
2023-09-08 15:42:46 +0900

    refactor: Add useCallback to prevent unnecessary re-renders

--- src/hooks/post.ts
+++ src/hooks/post.ts
@@ -1,4 +1,5 @@
 import { Comment, FullPost } from '@/model/post';
+import { useCallback } from 'react';
 import useSWR, { useSWRConfig } from 'swr';
 
 async function addComment(postId: string, comment: string) {
@@ -15,23 +16,26 @@ export default function useFullPost(postId: string) {
     error,
     mutate,
   } = useSWR<FullPost>(`/api/posts/${postId}`);
-
+  console.log(post);
   const { mutate: globalMutate } = useSWRConfig();
 
-  const postComment = (comment: Comment) => {
-    if (!post) return;
-    const newPost = {
-      ...post,
-      comments: [...post.comments, comment],
-    };
+  const postComment = useCallback(
+    (comment: Comment) => {
+      if (!post) return;
+      const newPost = {
+        ...post,
+        comments: [...post.comments, comment],
+      };
 
-    return mutate(addComment(post.id, comment.comment), {
-      optimisticData: newPost,
-      populateCache: false,
-      revalidate: false,
-      rollbackOnError: true,
-    }).then(() => globalMutate('/api/posts'));
-  };
+      return mutate(addComment(post.id, comment.comment), {
+        optimisticData: newPost,
+        populateCache: false,
+        revalidate: false,
+        rollbackOnError: true,
+      }).then(() => globalMutate('/api/posts'));
+    },
+    [post, mutate, globalMutate]
+  );
 
   return { post, isLoading, error, postComment };
 }
--- src/hooks/posts.ts
+++ src/hooks/posts.ts
@@ -1,4 +1,5 @@
 import { Comment, SimplePost } from '@/model/post';
+import { useCallback } from 'react';
 import useSWR from 'swr';
 
 async function updateLike(postId: string, like: boolean) {
@@ -24,40 +25,44 @@ export default function usePosts() {
     mutate,
   } = useSWR<SimplePost[]>('/api/posts');
 
-  // setLike를 호출하면 로컬상으로 업데이트 할 변경된 포스트의 배열을 새롭게 만듦
-  const setLike = (post: SimplePost, username: string, like: boolean) => {
-    const newPost = {
-      ...post,
-      likes: like
-        ? [...post.likes, username] // 사용자가 포스트를 like한다면, 해당 포스트의 likes 정보와 username의 배열을 추가
-        : post.likes.filter((item) => item !== username), // 사용자가 포스트를 like하지 않는다면, likes배열에서 사용자 username을 제거
-    };
-    // 기존의 post 데이터의 아이디가 newPost의 아이디와 같다면 newPost를 사용하고, 같지 않다면 기존의 post 데이터를 사용
-    const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+  const setLike = useCallback(
+    (post: SimplePost, username: string, like: boolean) => {
+      const newPost = {
+        ...post,
+        likes: like
+          ? [...post.likes, username]
+          : post.likes.filter((item) => item !== username),
+      };
 
-    // updateLike함수실생의 반환된 값으로 mutate
-    return mutate(updateLike(post.id, like), {
-      optimisticData: newPosts,
-      populateCache: false,
-      revalidate: false,
-      rollbackOnError: true,
-    });
-  };
+      const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
 
-  const postComment = (post: SimplePost, comment: Comment) => {
-    const newPost = {
-      ...post,
-      comments: post.comments + 1,
-    };
-    const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+      return mutate(updateLike(post.id, like), {
+        optimisticData: newPosts,
+        populateCache: false,
+        revalidate: false,
+        rollbackOnError: true,
+      });
+    },
+    [posts, mutate]
+  );
 
-    return mutate(addComment(post.id, comment.comment), {
-      optimisticData: newPosts,
-      populateCache: false,
-      revalidate: false,
-      rollbackOnError: true,
-    });
-  };
+  const postComment = useCallback(
+    (post: SimplePost, comment: Comment) => {
+      const newPost = {
+        ...post,
+        comments: post.comments + 1,
+      };
+      const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+
+      return mutate(addComment(post.id, comment.comment), {
+        optimisticData: newPosts,
+        populateCache: false,
+        revalidate: false,
+        rollbackOnError: true,
+      });
+    },
+    [posts, mutate]
+  );
 
   return { posts, isLoading, error, setLike, postComment };
 }
