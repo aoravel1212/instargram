32813734e7b6c1a4ec1758ff5964b5cba7704aaf
slevme <hby2233@naver.com>
2023-09-12 17:29:31 +0900

    refactor: Change the rendering of comments from the PostDetail component to the PostContent component

---
+++ src/components/PostContent.tsx
@@ -0,0 +1,75 @@
+import { FullPost } from '@/model/post';
+import Avatar from './Avatar';
+import { parseDate } from '@/util/date';
+
+type Props = {
+  image: string;
+  username: string;
+  data: FullPost;
+  createdAt: string;
+};
+
+export default function PostContent({
+  image,
+  username,
+  createdAt,
+  data,
+}: Props) {
+  return (
+    <div className="p-2 border-t border-gray-200 overflow-y-auto">
+      <div className="flex ">
+        <div className="p-1">
+          <Avatar image={image} size="small" highlight />
+        </div>
+        <div className="ml-2">
+          <div>
+            <span className="text-gray-900 font-bold">{username}</span>
+            <span className="ml-2">{data.text}</span>
+          </div>
+          <p className="text-xs text-neutral-500 uppercase my-2">
+            {parseDate(createdAt)}
+          </p>
+        </div>
+      </div>
+      <ul className="h-full mb-1">
+        {data.comments &&
+          data.comments.map(
+            (
+              { username: commentUsername, image, text: comment, createdAt },
+              index
+            ) => (
+              // <li key={index} className="flex items-center p-2">
+              //   <Avatar
+              //     image={image}
+              //     size="small"
+              //     highlight={commentUsername === username}
+              //   />
+              //   <div className="ml-4">
+              //     <span className="font-bold mr-2">{commentUsername}</span>
+              //     <span>{comment}</span>
+              //   </div>
+              // </li>
+              <li key={index} className="flex">
+                <div className="p-1">
+                  <Avatar
+                    image={image}
+                    size="small"
+                    highlight={commentUsername === username}
+                  />
+                </div>
+                <div className="ml-2">
+                  <div>
+                    <span className="text-gray-900 font-bold">{username}</span>
+                    <span className="ml-2">{comment}</span>
+                  </div>
+                  <p className="text-xs text-neutral-500 uppercase my-2">
+                    {parseDate(createdAt)}
+                  </p>
+                </div>
+              </li>
+            )
+          )}
+      </ul>
+    </div>
+  );
+}
--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -2,17 +2,17 @@ import { SimplePost } from '@/model/post';
 import Image from 'next/image';
 import PostUserAvatar from './PostUserAvatar';
 import ActionBar from './ActionBar';
-import Avatar from './Avatar';
 import useFullPost from '@/hooks/post';
+import PostContent from './PostContent';
 
 type Props = {
   post: SimplePost;
 };
 
 export default function PostDetail({ post }: Props) {
-  const { id, username, userImage, image } = post;
+  const { id, username, userImage, image, createdAt } = post;
   const { post: data, postComment } = useFullPost(id);
-  const comments = data?.comments;
+  // const comments = data?.comments;
 
   return (
     <section className="flex w-full h-full">
@@ -28,24 +28,14 @@ export default function PostDetail({ post }: Props) {
       </div>
       <div className="w-full basis-2/5 flex flex-col">
         <PostUserAvatar image={userImage} username={username} />
-        <ul className="border-t border-gray-200 h-full overflow-y-auto p-4 mb-1">
-          {comments &&
-            comments.map(
-              ({ username: commentUsername, image, text: comment }, index) => (
-                <li key={index} className="flex items-center mb-1">
-                  <Avatar
-                    image={image}
-                    size="small"
-                    highlight={commentUsername === username}
-                  />
-                  <div className="ml-2">
-                    <span className="font-bold mr-1">{commentUsername}</span>
-                    <span>{comment}</span>
-                  </div>
-                </li>
-              )
-            )}
-        </ul>
+        {data && (
+          <PostContent
+            image={userImage}
+            username={username}
+            createdAt={createdAt}
+            data={data}
+          />
+        )}
         <ActionBar post={post} onComment={postComment} />
       </div>
     </section>
