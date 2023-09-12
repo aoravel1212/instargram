cea178a26743e2d64f84eb8d0e8157e904c3033a
slevme <hby2233@naver.com>
2023-09-12 16:25:58 +0900

    refactor: Change the language of the timeago.js library from English to Korean

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,5 +1,8 @@
-import { format } from 'timeago.js';
+import { format, register } from 'timeago.js';
+import koLocale from 'timeago.js/lib/lang/ko';
+
+register('ko', koLocale);
 
 export function parseDate(date: string) {
-  return format(date);
+  return format(date, 'ko');
 }
