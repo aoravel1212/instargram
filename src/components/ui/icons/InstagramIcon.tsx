6f451370095850df7f56265e479d34d5d39108e2
slevme <hby2233@naver.com>
2024-02-09 21:47:42 +0900

    design: Add new icons

---
+++ src/components/ui/icons/InstagramIcon.tsx
@@ -0,0 +1,5 @@
+import { LuInstagram } from 'react-icons/lu';
+
+export default function InstagramIcon() {
+  return <LuInstagram className="" />;
+}
---
+++ src/components/ui/icons/SignOutIcon.tsx
@@ -0,0 +1,5 @@
+import { PiSignOut } from 'react-icons/pi';
+
+export default function SignOutIcon() {
+  return <PiSignOut className="w-6 h-6" />;
+}
