1362caabdbdc5a3e154351514c07a4ba6f69f7e0
slevme <hby2233@naver.com>
2024-02-10 00:34:24 +0900

    feat: Add TopBar component for mobile screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import TopBar from '@/components/TopBar';
 import './globals.css';
 import Navbar from '@/components/Navbar';
 import AuthContext from '@/context/AuthContext';
@@ -24,10 +25,15 @@ export default function RootLayout({
     <html lang="en" className={openSans.className}>
       <body className="w-full bg-neutral-50 overflow-auto">
         <AuthContext>
-          <header className="fixed z-10 bottom-0 md:left-0 h-12 md:h-full w-full md:w-16 lg:w-48 xl:w-64 border-t md:border-t-0 md:border-r bg-white">
-            <Navbar />
+          <header>
+            <div className="z-10 fixed top-0 w-full h-[60px] bg-white border-b md:hidden">
+              <TopBar />
+            </div>
+            <div className="z-10 fixed bottom-0 md:left-0 h-12 md:h-full w-full md:w-16 lg:w-48 xl:w-64 border-t md:border-t-0 md:border-r bg-white">
+              <Navbar />
+            </div>
           </header>
-          <main className="w-full flex justify-center max-w-screen-xl mx-auto">
+          <main className="w-full flex justify-center max-w-screen-xl mx-auto mt-[60px] md:mt-0">
             <SWRConfigContext>{children}</SWRConfigContext>
           </main>
         </AuthContext>
---
+++ src/components/TopBar.tsx
@@ -0,0 +1,7 @@
+export default function TopBar() {
+  return (
+    <>
+      <h1 className="text-xl font-semibold p-4">Instargram</h1>
+    </>
+  );
+}
