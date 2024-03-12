135e6fad6f358ff38b3b612f4f881cbd4895c7b4
slevme <hby2233@naver.com>
2024-03-12 22:31:45 +0900

    refactor: Replace InstagramLogoIcon with LogoIcon and add logout functionality to TopBar component

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,5 +1,16 @@
-import InstagramLogoIcon from './ui/icons/InstagramLogoIcon';
+import LogoIcon from './ui/icons/LogoIcon';
+import SignOutIcon from './ui/icons/SignOutIcon';
+import AuthButton from './ui/AuthButton';
 
 export default function TopBar() {
-  return <InstagramLogoIcon componentType="TopBar" />;
+  return (
+    <div className="flex justify-between place-content-center">
+      <LogoIcon componentType="TopBar" />
+      <AuthButton>
+        <div className="p-4">
+          <SignOutIcon />
+        </div>
+      </AuthButton>
+    </div>
+  );
 }
