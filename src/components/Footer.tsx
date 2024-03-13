331af17c571e5cba38da76e7ce72b6ed3e1bc84b
slevme <hby2233@naver.com>
2024-03-14 01:02:44 +0900

    design: Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center my-4">
+      <p className="font-bold text-sm text-neutral-500">
+        @2024 INSTARGRAM from MEATAL
+      </p>
+    </footer>
+  );
+}
