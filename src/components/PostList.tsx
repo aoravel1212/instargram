4ec76bc2d21de4ed60c71d2ef47bc78f2b3445d0
slevme <hby2233@naver.com>
2024-03-18 01:15:07 +0900

    design: Ensure responsive width for PostList component

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -8,14 +8,14 @@ export default function PostList() {
   const { posts, isLoading, error } = usePosts();
 
   return (
-    <section>
+    <section className="max-w-full" style={{ width: 'min(470px, 100vw)' }}>
       {isLoading && (
         <div className="text-center mt-32">
           <GridSpinner color="red" />
         </div>
       )}
       {posts && (
-        <ul>
+        <ul className="w-full">
           {posts
             ? posts.map((post, index) => (
                 <li key={post.id} className="mb-4">
